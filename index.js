3a9ebe3f13447f320f35d45f9a8527137133750a
Soichiro Shishido <shishidosoichiro@gmail.com>
2016-06-16 07:44:44 +0900

    not yet

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var Transform = stream.Transform;
 var http = require('http');
 var util = require('util');
 var Url = require('url2');
+var _ = require('lodash');
 
 var App = module.exports = function(url){
 	if (!(this instanceof App)) return new App(url);
@@ -57,22 +58,27 @@ App.prototype.post = function(url, data){
 	// var promise = curried(object);
 	else if (arguments.length === 1 && typeof arguments[0] === 'string') {
 		url = arguments[0];
-		var curried = function(data){
+		var write = function(data){
 			return this.post(url, data);
 		}.bind(this);
 		// extend Writable interface.
-		var Dummy = function(){};
-		Dummy.prototype = curried;
-		util.inherits(Dummy, Writable);
-		Writable.call(curried, {objectMode: true, write: curried});
-		return curried;
+		var Post = function(){
+			Writable.call(this, {objectMode: true, write: write});
+		};
+		util.inherits(Post, Writable);
+		var post = new Post();
+		for (var i in post) {
+			if (typeof post[i] === 'function') write[i] = post[i].bind(post);
+			else write[i] = post[i];
+		}
+		return write;
 	}
 	// Invalid arguments.
 	else throw Error('invalid arguments.');
 
 	// post a request.
 	var urlObj = this.url.resolve(url);
-	var reqestOptions = _.defaults({method: 'post'}, urlObj);
+	var reqestOptions = _.defaults({method: 'POST', headers: {'Content-Type': 'application/json'}}, urlObj);
 	var options = _.defaults(this.options || {}, {encoding: 'utf8'});
 	return new Promise(function(resolve, reject){
 		var req = http.request(reqestOptions, function(res){
@@ -93,7 +99,7 @@ App.prototype.post = function(url, data){
 		});
 
 		// write data to request body
-		req.write(data);
+		req.write(JSON.stringify(data));
 		req.end();
 	});
 };
--- test/index.js
+++ test/index.js
@@ -3,10 +3,14 @@
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
-
 var express = require('express')
+var bodyParser = require('body-parser');
+
 var server = express()
+.use(bodyParser.json())
+.use(bodyParser.urlencoded({ extended: true }))
 .post('/api/user', function(req, res){
+	console.log('yes!')
 	console.log(req.body)
 	res.send(req.body);
 })
@@ -16,24 +20,29 @@ var stream = require('stream');
 
 var Req = require('../');
 
+var array = function(data){
+	return new stream.Readable({
+		objectMode: true,
+		read: function(){
+			this.push(data.shift())
+		}
+	});
+};
+
 describe('req', function(){
 	describe('#post', function(){
-		it('should ', function(){
+		it('should ', function(done){
 			var req = Req('http://localhost:3000/api');
 
-			var data = [
-				{username: 'user1'},
-				{username: 'user2'}
-			]
-			new stream.Readable({
-				read: function(){
-					this.push(data.shift())
-				}
+			var data = [{username: 'user1'}, {username: 'user2'}];
+			array(data)
+			.pipe(req.post('api/user'))
+			.on('end', function(){
+				done()
 			})
-			.pipe(req.post('user'))
-			.then(function(res){
-				res.body.should.deep.equal(data[0])
-			})
+			//.then(function(res){
+			//	res.body.should.deep.equal(data[0])
+			//})
 		});
 	});
 });
