bcfb2366d7e52305d14ecf906ed034a10538105e
Soichiro Shishido <shishidosoichiro@gmail.com>
2016-06-15 10:32:05 +0900

    typing

--- index.js
+++ index.js
@@ -1,25 +1,99 @@
+'use strict';
+
 var stream = require('stream');
+var Writable = stream.Writable;
+var Transform = stream.Transform;
 var http = require('http');
+var util = require('util');
 var Url = require('url2');
 
 var App = module.exports = function(url){
+	if (!(this instanceof App)) return new App(url);
 	this.url = Url(url);
+	this.post = this.post.bind(this);
 };
 
 /**
-var promise = req.post(data);
-var promise = req.post(url, data);
-var promise = req.post(url);
-var writable = req.post(url);
-var curried = req.post(url);
-var promise = curried(data);
-
+ * post
+ *
+ * Return Promise pattern.
+ *
+ * ```js
+ * var promise = req.post(string, object);
+ * ```
+ *
+ *
+ * Return Promise pattren omitting url.
+ *
+ * ```js
+ * var promise = req.post(object);
+ * ```
+ *
+ *
+ * Return curried pattren with Writable.
+ *
+ * ```js
+ * var writable = req.post(string);
+ * var curried = req.post(string);
+ * var promise = curried(object);
+ * ```
+ *
  */
 App.prototype.post = function(url, data){
-	//if (arguments.length === 1 && typefo url === 'string')　
-	//else if (arguments.length === 1 && typeof url === 'object')
-	var urlObj = this.url.resolve(url);
-	var options = _.defaults({method: 'post'}, urlObj);
-	var req = http.request(options);
+	// Return promise pattern.
+	// var promise = req.post(string, object);
+	if (arguments.length === 2 && typeof arguments[0] === 'string' && typeof arguments[1] === 'object') {
+		// Follows 'if ... else ...' clauses.
+	}
+	// Return promise pattren omitting url.
+	// var promise = req.post(object);
+	else if (arguments.length === 1 && typeof arguments[0] === 'object') {
+		return this.post('', arguments[0]);
+	}
+	// Return curried pattren with Writable.
+	// var promise = req.post(string);
+	// var writable = req.post(string);
+	// var curried = req.post(string);
+	// var promise = curried(object);
+	else if (arguments.length === 1 && typeof arguments[0] === 'string') {
+		url = arguments[0];
+		var curried = function(data){
+			return this.post(url, data);
+		}.bind(this);
+		// extend Writable interface.
+		var Dummy = function(){};
+		Dummy.prototype = curried;
+		util.inherits(Dummy, Writable);
+		Writable.call(curried, {objectMode: true, write: curried});
+		return curried;
+	}
+	// Invalid arguments.
+	else throw Error('invalid arguments.');
 
+	// post a request.
+	var urlObj = this.url.resolve(url);
+	var reqestOptions = _.defaults({method: 'post'}, urlObj);
+	var options = _.defaults(this.options || {}, {encoding: 'utf8'});
+	return new Promise(function(resolve, reject){
+		var req = http.request(reqestOptions, function(res){
+			// console.log(`STATUS: ${res.statusCode}`);
+			// console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+			res.setEncoding(options.encoding);
+			var data = [];
+			res.on('data', function(chunk){
+				data = data.push(chunk);
+			});
+			res.on('end', function(){
+				resolve(data.join(''));
+			});
+		});
+
+		req.on('error', function(e){
+			reject(e);
+		});
+
+		// write data to request body
+		req.write(data);
+		req.end();
+	});
 };
